2c4709f9ff924dabb4fbafb43691a772f6078629
ViktorHolovanets <victorgolova@gmail.com>
2023-06-21 18:44:34 +0300

    1

--- node_auth_jwt/src/index.js
+++ node_auth_jwt/src/index.js
@@ -6,12 +6,12 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
-app.use(bodyParser.json()); // обробляє JSON запити
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router)
 const serverName = process.env.NAME || 'Nodejs JWT Auth';
 const PORT = process.env.PORT || 3000;
 
 
-// Запустил сервер
+
 app.listen(PORT, () => console.log("Server " + serverName + " running at port " + PORT));
